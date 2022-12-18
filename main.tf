b168bfa51eff442992133da3ccce6d157a44a4a6
ahd3r <ander11110@gmail.com>
2022-12-18 19:50:28 +0100

    fix

--- main.tf
+++ main.tf
@@ -8,14 +8,20 @@ variable "aws_secret_key" {
     type = string
 }
 
+variable "s3_resource_name" {
+    description = "S3 bucket name"
+    type = string
+    default = "firebase-static-222gd548241h52s5"
+}
+
 provider "aws" {
     access_key = "${var.aws_access_key}"
     secret_key = "${var.aws_secret_key}"
     region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "firebase-static-fdhjga" {
-    bucket = "${aws_s3_bucket.resource_name}"
+resource "aws_s3_bucket" "$${var.s3_resource_name}" {
+    bucket = "${var.s3_resource_name}"
     acl = "public-read"
     policy = <<-EOF
 {
@@ -28,7 +34,7 @@ resource "aws_s3_bucket" "firebase-static-fdhjga" {
                 "AWS": "*"
             },
             "Action": "s3:GetObject",
-            "Resource": "arn:aws:s3:::firebase-static-gd548241h52s5/*"
+            "Resource": "arn:aws:s3:::${var.s3_resource_name}/*"
         }
     ]
 }
